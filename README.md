b25574974ceca3ecf7c54537f7f8f29468fd51d1
XIA <36402684+VenomousXIA@users.noreply.github.com>
2021-10-23 15:11:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Landing Page Project
+
+## Table of Contents
+
+- [Description](#Description)
+- [Dependencies](#Dependencies)
+- [Instructions](#instructions)
+
+## Description
+
+This is a project for the Udacity FWD web development classroom **Professional Track**
+
+## Dependencies
+
+No Thirdparty tools required! just open the website and have some fun reading paragraphs.
+
+## Instructions
+
+1. Open Website
+2. Choose/Scroll to the section you want to read
+
+## Contributors
+
+This project was built on the static template provided by the project resources and edited by **Abdallah Ahmed**
---
+++ js/app.js
@@ -0,0 +1,123 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+// Global Variables
+
+const navMenu = document.querySelector('.navbar__menu');
+const mainElement = document.querySelector('main');
+let sections;
+
+// Functions
+
+/**
+ * Updates the sections array to include any new sections
+ */
+function updateSections() {
+	sections = Array.from(mainElement.children).filter(
+		(element) => element.nodeName == 'SECTION'
+	);
+}
+
+function isSectionAdded() {
+	const currentSections = Array.from(mainElement.children).filter(
+		(element) => element.nodeName == 'SECTION'
+	);
+
+	return sections.length !== currentSections.length;
+}
+
+/**
+ * Function that fetches all the sections in the website and
+ * builds a nav item refering to each one
+ */
+function buildNavMenu() {
+	const navFragment = document.createDocumentFragment();
+
+	let navMenuItem;
+	for (let section of sections) {
+		navMenuItem = document.createElement('ul');
+		navMenuItem.innerHTML = `<a href='#${section.id}' class='menu__link'>${section.id}</a>`;
+		navFragment.appendChild(navMenuItem);
+	}
+	navMenu.appendChild(navFragment);
+}
+
+/**
+ * Updates the nav menu on adding new sections
+ */
+function updateNavMenu() {
+	const addedSections = sections.slice(navMenu.children.length);
+
+	navMenu.style.display = 'none';
+
+	let navMenuItem;
+	for (let section of addedSections) {
+		navMenuItem = document.createElement('ul');
+		navMenuItem.innerHTML = `<a href='#${section.id}' class='menu__link'>${section.id}</a>`;
+		navMenu.appendChild(navMenuItem);
+	}
+	navMenu.style.display = 'block';
+}
+
+/**
+ * Function that checks all the sections locations
+ * and determines the top most section as the active one
+ * Section location is detected by it's header and last paragraph locations
+ */
+function setActiveSection() {
+	sections?.forEach((section) => {
+		const sectionDiv = section.firstElementChild;
+		const startPosition = sectionDiv.firstElementChild.getBoundingClientRect();
+		const endPosition = sectionDiv.lastElementChild.getBoundingClientRect();
+
+		startPosition.top <= window.innerHeight / 4 && endPosition.bottom >= 0
+			? section.classList.add('active__section')
+			: section.classList?.remove('active__section');
+	});
+}
+
+/**
+ * Initializes the webpage on load
+ */
+function init() {
+	updateSections();
+	buildNavMenu();
+}
+
+// Code Main Body
+
+init();
+
+// Events and Listeners
+
+/**
+ * Updates the current active section
+ * Updates the nav menu on adding new sections
+ */
+window.addEventListener(
+	'scroll',
+	() =>
+		setTimeout(() => {
+			setActiveSection();
+
+			if (isSectionAdded()) {
+				updateSections();
+				updateNavMenu();
+			}
+		}, 0),
+	{
+		passive: true,
+	}
+);
